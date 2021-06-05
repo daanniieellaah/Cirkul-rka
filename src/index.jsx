bf9c35ba11b1d64653231f9b6c41ac26dedd75a1
limgasimga <simonabohusova@gmail.com>
2021-06-05 15:55:14 +0200

    Filtr - checkboxy zmeny

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 import './style.css';
-<<<<<<< HEAD
 import {VenueDetail, VenueList, Filtr} from './components';
-=======
-import {VenueDetail, VenueList} from './components';
->>>>>>> 92c52bc5cf109c48755743f1eb40f8f75f0c308e
 import {getVenues, getCategories} from './mocks';
 
 const venuesList = getVenues();
@@ -22,12 +18,7 @@ const App = () => {
     <>
     <VenueDetail data={venue}/>
     <VenueList venues={venuesList} handleVenue={setVenue}/>
-<<<<<<< HEAD
     <Filtr categories={categories}/>
-=======
-    
-
->>>>>>> 92c52bc5cf109c48755743f1eb40f8f75f0c308e
     </>  
      </div>
 
