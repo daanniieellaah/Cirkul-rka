6becc18ff4c9eb7c46fccc09007b95795107ead2
limgasimga <simonabohusova@gmail.com>
2021-06-05 18:10:04 +0200

    ItemCategories2

--- src/components/venue_list/index.jsx
+++ src/components/venue_list/index.jsx
@@ -2,34 +2,25 @@ import React from 'react';
 import {Address} from '..'
 
 
-const VenueList = (props) => {
-   
+cconst VenueList = (props) => {
     const handleClick = (key) => {
         props.handleVenue(props.venues[key])
     };
-
-    const places = props.venues.filter((item)=>{
-        if ( props.venues.kategorie.includes(item)
-        return
-    })
-
-    return (
-        
+const places = props.venues.filter((item) => {
+   return props.selectedCategories.includes (item.kategorie)
+} )
+return (
     <div className="list">
-
-        <ul>{props.venues.map((item, i) =>
-        
+        <ul>{places.map((item, i) =>
             <li key={i}>
                 <a   href="#" onClick={() => handleClick(i)}>
                 {item.nazev}
                 <Address data={item.adresa} img ={item.image_url}/>
                 </a>
             </li>
-        
         )}
         </ul>
     </div>
     )  
 };
-
 export default VenueList;
