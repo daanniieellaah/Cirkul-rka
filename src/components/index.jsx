b757043c5f62a743ae53d0e57a048f7f66fa42ee
limgasimga <simonabohusova@gmail.com>
2021-06-05 19:01:28 +0200

    Mapa2

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,5 +3,6 @@ import VenueList from './venue_list';
 import Address from './address';
 import Filtr from "./Filtr";
 import ItemCategories from "./ItemCategories";
+import Mapa from "./Venue_map"
 
-export  {VenueDetail, VenueList, Address, Filtr, ItemCategories};
+export  {VenueDetail, VenueList, Address, Filtr, ItemCategories, Mapa};
