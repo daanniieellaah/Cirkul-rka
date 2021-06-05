7453258de9367bc670bf2dd9f812ae6f8eeea836
Daniela Horáková <hu.dani.hu@gmail.com>
2021-06-05 19:43:26 +0200

    vykreslení mapy - začátek

--- src/components/Venue_map/index.jsx
+++ src/components/Venue_map/index.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
 import ReactMapGL from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function Mapa() {
   const [viewport, setViewport] = useState({
@@ -10,13 +11,39 @@ function Mapa() {
     longitude: -122.4376,
     zoom: 8
   });
-
+  
   return (
     <ReactMapGL
-      {...viewport}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+				mapStyle={{
+					version: 8,
+					sources: {
+						'raster-tiles': {
+							type: 'raster',
+							tiles: ['https://mapserver.mapy.cz/base-m/{z}-{x}-{y}'],
+							tileSize: 256,
+							attribution:
+								'Mapové podklady od <a target="_top" rel="noopener" href="https://mapy.cz/">Seznam.cz</a> a <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>.',
+						},
+					},
+					layers: [
+						{
+							id: 'simple-tiles',
+							type: 'raster',
+							source: 'raster-tiles',
+							minzoom: 0,
+							maxzoom: 20,
+						},
+					],
+				}}
+				{...viewport}
+				onViewportChange={(nextViewport) => setViewport(nextViewport)}
+				width="100%"
+				height="450px"
+			/>
   );
-}
+      };
+
+  
+
 
 export default Mapa
