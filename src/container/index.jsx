f16f9f341c363e00d472d3b1cbced170bed6710f
Daniela Horáková <81040430+daanniieellaah@users.noreply.github.com>
2021-06-06 10:32:08 +0200

    Update index.jsx

--- src/container/index.jsx
+++ src/container/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import {Mapa, VenueList, Filtr} from '..';
+import {Mapa, VenueList} from '..';
 
-const Container = () => {
+const Container = (props) => {
+  
+const places = props.venues.filter((item) => {
+    return props.selectedCategories.includes (item.kategorie)
+ } )
 
-
+return (
+  <div>
+    <VenueList venues={places} selectedCategories={selectedCategories} handleVenue={setVenue}/>
+    <Mapa/>
+  </div>
+)
 };
 
 
