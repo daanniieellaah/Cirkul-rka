8ccc5bd6b7a73451796979c10d0cbffffd719a19
limgasimga <simonabohusova@gmail.com>
2021-06-07 20:08:01 +0200

    Úvodná stránka plus basic CSS

--- src/index.jsx
+++ src/index.jsx
@@ -12,9 +12,6 @@ import {
   Link
 } from 'react-router-dom';
 
-
-
-
 const venuesList = getVenues();
 const categories = getCategories();
 
@@ -28,29 +25,38 @@ const App = () => {
   
   return (
     <Router>
-    <div className="container">
-      <Menu/>
-    <Switch>
-    <Route  exact path= "/">
-    <div>Úvodní stránka</div>
-    
-      </Route>
+      <div className="container">
+        <Menu/>
 
-      <Route path="/mapa">
-        <>
-      <VenueDetail data={venue}/>
-        <Filtr categories={categories} selectedCategories={selectedCategories} setSelectedCategories={setSelectedCategories}/> 
-    <Container venues={venuesList} selectedCategories={selectedCategories} handleVenue={setVenue}/>
-    </>
-      </Route>
+        <Switch>
 
-    
-    </Switch>
+        <Route  exact path= "/">
+          <div className="obsah">
+            <div className="logo__stred">
+              <img src="img/final.png"/>
+            </div>
+            <div className="banner">
+              <p className="popis"> Cirkulárna mapa mesta Trenčín</p>
+            </div>
+          </div>    
+        </Route>
+
+        <Route path="/mapa">
+          <>
+            <VenueDetail data={venue}/>
+
+            <Filtr categories={categories} selectedCategories={selectedCategories} setSelectedCategories={setSelectedCategories}/>
+
+            <Container venues={venuesList} selectedCategories={selectedCategories} handleVenue={setVenue}/>
+          </>
+        </Route>
+        
+        </Switch>
+
         <Footer/>
 
-    
-     </div>
-     </Router>
+      </div>
+    </Router>
   )
 };
 
