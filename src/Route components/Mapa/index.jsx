a0acd3d60b35587fdc5709826aa7e3b54878324f
limgasimga <simonabohusova@gmail.com>
2021-06-10 20:59:03 +0200

    Mapakomponenta

---
+++ src/Route components/Mapa/index.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { VenueDetail, Filtr, Container} from '/src/components';
+import { getVenues, getCategories } from '/src/mocks';
+
+
+const venuesList = getVenues();
+const categories = getCategories();
+
+const Mapakomponenta = () => {
+  const [venue, setVenue] = useState(venuesList[0]);
+  const categoriesKeyes = categories.map((item) => {
+    return item.typ;
+  });
+  const [selectedCategories, setSelectedCategories] = useState(categoriesKeyes);
+
+  return (
+        
+            <>
+              <VenueDetail data={venue} />
+
+              <Filtr
+                categories={categories}
+                selectedCategories={selectedCategories}
+                setSelectedCategories={setSelectedCategories}
+              />
+
+              <Container
+                venues={venuesList}
+                selectedCategories={selectedCategories}
+                handleVenue={setVenue}
+              />
+            </>
+         
+  );
+};
+
+export default Mapakomponenta;
--- src/index.jsx
+++ src/index.jsx
@@ -2,26 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 import './style.css';
 import { VenueDetail, Filtr, Container, Footer, Menu } from './components';
-import { getVenues, getCategories } from './mocks';
 
 
 import CirkularniEkonomika from './Route components/CirkularniEkonomika';
 import Onas from './Route components/Onas';
 import Kontakt from './Route components/Kontakt';
 import Home from './Route components/Home';
+import Mapakomponenta from "./Route components/Mapa";
 
 
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-const venuesList = getVenues();
-const categories = getCategories();
-
 const App = () => {
-  const [venue, setVenue] = useState(venuesList[0]);
-  const categoriesKeyes = categories.map((item) => {
-    return item.typ;
-  });
-  const [selectedCategories, setSelectedCategories] = useState(categoriesKeyes);
 
   return (
     <Router>
@@ -34,21 +26,7 @@ const App = () => {
           </Route>
 
           <Route path="/mapa">
-            <>
-              <VenueDetail data={venue} />
-
-              <Filtr
-                categories={categories}
-                selectedCategories={selectedCategories}
-                setSelectedCategories={setSelectedCategories}
-              />
-
-              <Container
-                venues={venuesList}
-                selectedCategories={selectedCategories}
-                handleVenue={setVenue}
-              />
-            </>
+            <Mapakomponenta/>
           </Route>
 
           <Route path="/onas">
